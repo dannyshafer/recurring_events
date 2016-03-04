6d5603b4cd6aceb05f3c8611074a86a1ed92f9a8
Daniel Shafer <github@dannyshafer.com>
2016-03-04 14:32:04 -0800

    improve appearance and help future deliveries action account for changing years

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -11,7 +11,7 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
-    @delivery_dates = @event.delivery_dates
+    @delivery_dates = @event.delivery_dates.compact
   end
 
   # GET /events/new
@@ -30,13 +30,11 @@ class EventsController < ApplicationController
     @event.day_of_month = @event.start_date.to_s.split("-").last
     respond_to do |format|
       if @event.save
-        #start calculating delivery dates
         if is_not_holiday(@event.start_date) && is_weekday(@event.start_date)
           DeliveryDate.create(delivery: @event.start_date, event_id: @event.id)
           future_deliveries(@event.start_date, @event.occurence_frequency)
         else
           #adjust the date by one and make delivery date
-          puts "boo"*100
         end
         format.html { redirect_to @event, notice: 'Event was successfully created.' }
         format.json { render :show, status: :created, location: @event }
@@ -52,6 +50,13 @@ class EventsController < ApplicationController
   def update
     respond_to do |format|
       if @event.update(event_params)
+        @event.delivery_dates.destroy_all
+        if is_not_holiday(@event.start_date) && is_weekday(@event.start_date)
+          DeliveryDate.create(delivery: @event.start_date, event_id: @event.id)
+          future_deliveries(@event.start_date, @event.occurence_frequency)
+        else
+          #adjust the date by one and make delivery date
+        end
         format.html { redirect_to @event, notice: 'Event was successfully updated.' }
         format.json { render :show, status: :ok, location: @event }
       else
@@ -86,6 +91,7 @@ class EventsController < ApplicationController
       if date != "holiday" #come back to this later and do a real holiday check
         true
       else
+        #returns a date that is not a holiday
         false
       end
     end
@@ -94,6 +100,7 @@ class EventsController < ApplicationController
       if !date.sunday? && !date.saturday? 
         true
       else
+        #returns a date that is a weekday
         false
       end
     end
@@ -101,9 +108,14 @@ class EventsController < ApplicationController
     def future_deliveries(date, occurence_frequency)
       counter = 1
       4.times do
-        @date = date.to_s.split("-")
-        @date[1] = (@date[1].to_i + occurence_frequency*counter).to_s
-        @date = @date.join("-")
+        @date = date.to_s.split("-").map(&:to_i)
+        @date[1] = (@date[1] + occurence_frequency*counter)
+        while @date[1] > 12
+          @date[1] -= 12
+          @date[0] += 1
+        end
+        @date = @date.map(&:to_s).join("-")
+        #check if its a holiday or weekend
         DeliveryDate.create(delivery: @date, event_id: @event.id)
         counter += 1
       end
